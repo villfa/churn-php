635701e89c46ec26c714010d05366c9096f6f84d
Fabien Villepinte <fabien.villepinte@gmail.com>
2022-02-04 23:28:34 +0100

    Try new implementation

--- src/Assessor/CyclomaticComplexityAssessor.php
+++ src/Assessor/CyclomaticComplexityAssessor.php
@@ -17,134 +17,50 @@ class CyclomaticComplexityAssessor
     private $score = 0;
 
     /**
-     * Asses the files cyclomatic complexity.
+     * Assess the files cyclomatic complexity.
      *
      * @param string $contents The contents of a PHP file.
      */
     public function assess(string $contents): int
     {
-        $this->score = 0;
-        $this->hasAtLeastOneMethod($contents);
-        $this->countTheIfStatements($contents);
-        $this->countTheElseIfStatements($contents);
-        $this->countTheWhileLoops($contents);
-        $this->countTheForLoops($contents);
-        $this->countTheCaseStatements($contents);
-        $this->countTheTernaryOperators($contents);
-        $this->countTheLogicalAnds($contents);
-        $this->countTheLogicalOrs($contents);
-
-        if (0 === $this->score) {
-            $this->score = 1;
+        $tokens = \token_get_all($contents);
+        $score = 0;
+        foreach ($tokens as $token) {
+            if (\is_array($token)) {
+                $score += $this->getComplexity($token[0]);
+            } elseif ($token === '?') {
+                $score += 1;
+            }
         }
 
-        return $this->score;
+        return $score === 0 ? 1 : $score;
     }
 
     /**
-     * Does the class have at least one method?
-     *
-     * @param string $contents File contents.
+     * @param int $code Code of a PHP token.
      */
-    private function hasAtLeastOneMethod(string $contents): void
+    private function getComplexity(int $code): int
     {
-        \preg_match("/[ ]function[ ]/", $contents, $matches);
-
-        if (!isset($matches[0])) {
-            return;
+        switch ($code) {
+            case T_CLASS:
+            case T_INTERFACE:
+            case T_TRAIT:
+            case 336: // T_ENUM (PHP 8.1)
+            case T_IF:
+            case T_ELSEIF:
+            case T_FOR:
+            case T_FOREACH:
+            case T_WHILE:
+            case T_CASE:
+            case T_CATCH:
+            case T_BOOLEAN_AND:
+            case T_LOGICAL_AND:
+            case T_BOOLEAN_OR:
+            case T_LOGICAL_OR:
+                return 1;
+            
+            default:
+                return 0;
         }
-
-        $this->score++;
-    }
-
-    /**
-     * Count how many if statements there are.
-     *
-     * @param string $contents File contents.
-     */
-    private function countTheIfStatements(string $contents): void
-    {
-        $this->score += $this->howManyPatternMatches("/[ ]if[ ]{0,}\(/", $contents);
-    }
-
-    /**
-     * Count how many else if statements there are.
-     *
-     * @param string $contents File contents.
-     */
-    private function countTheElseIfStatements(string $contents): void
-    {
-        $this->score += $this->howManyPatternMatches("/else[ ]{0,}if[ ]{0,}\(/", $contents);
-    }
-
-    /**
-     * Count how many while loops there are.
-     *
-     * @param string $contents File contents.
-     */
-    private function countTheWhileLoops(string $contents): void
-    {
-        $this->score += $this->howManyPatternMatches("/while[ ]{0,}\(/", $contents);
-    }
-
-    /**
-     * Count how many for loops there are.
-     *
-     * @param string $contents File contents.
-     */
-    private function countTheForLoops(string $contents): void
-    {
-        $this->score += $this->howManyPatternMatches("/[ ]for(each){0,1}[ ]{0,}\(/", $contents);
-    }
-
-    /**
-     * Count how many case statements there are.
-     *
-     * @param string $contents File contents.
-     */
-    private function countTheCaseStatements(string $contents): void
-    {
-        $this->score += $this->howManyPatternMatches("/[ ]case[ ]{1}(.*)\:/", $contents);
-    }
-
-    /**
-     * Count how many ternary operators there are.
-     *
-     * @param string $contents File contents.
-     */
-    private function countTheTernaryOperators(string $contents): void
-    {
-        $this->score += $this->howManyPatternMatches("/[ ]\?.*:.*;/", $contents);
-    }
-
-    /**
-     * Count how many '&&' there are.
-     *
-     * @param string $contents File contents.
-     */
-    private function countTheLogicalAnds(string $contents): void
-    {
-        $this->score += $this->howManyPatternMatches("/[ ]&&[ ]/", $contents);
-    }
-
-    /**
-     * Count how many '||' there are.
-     *
-     * @param string $contents File contents.
-     */
-    private function countTheLogicalOrs(string $contents): void
-    {
-        $this->score += $this->howManyPatternMatches("/[ ]\|\|[ ]/", $contents);
-    }
-
-    /**
-     * For the given $pattern on $string, how many matches are returned?
-     *
-     * @param string $pattern Regex pattern.
-     * @param string $string Any string.
-     */
-    private function howManyPatternMatches(string $pattern, string $string): int
-    {
-        return (int) \preg_match_all($pattern, $string);
     }
 }
