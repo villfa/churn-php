27cb4ef0cfc23aa97eea5f115b5ee3c70e92dc2e
Fabien Villepinte <fabien.villepinte@gmail.com>
2022-02-04 11:04:04 +0100

    Add assertion

--- tests/Integration/ManifestTest.php
+++ tests/Integration/ManifestTest.php
@@ -13,6 +13,8 @@ class ManifestTest extends BaseTestCase
     public function manifest_is_valid()
     {
         $path = __DIR__ . '/../../manifest.xml';
+        $this->assertTrue(is_file($path), 'manifest.xml not found');
+
         $manifest = ManifestLoader::fromFile($path);
 
         $this->assertEquals('bmitch/churn-php', $manifest->getName());
