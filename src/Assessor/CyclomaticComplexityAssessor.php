9cbfdca97bf33921627ddce301c9f7944e2c46ca
Fabien Villepinte <fabien.villepinte@gmail.com>
2022-03-03 15:46:33 +0000

    Refactor CyclomaticComplexityAssessorTest

--- src/Assessor/CyclomaticComplexityAssessor.php
+++ src/Assessor/CyclomaticComplexityAssessor.php
@@ -10,13 +10,6 @@ namespace Churn\Assessor;
 class CyclomaticComplexityAssessor
 {
     /**
-     * The total cyclomatic complexity score.
-     *
-     * @var integer
-     */
-    private $score = 0;
-
-    /**
      * Assess the files cyclomatic complexity.
      *
      * @param string $contents The contents of a PHP file.
@@ -28,37 +21,39 @@ class CyclomaticComplexityAssessor
         foreach ($tokens as $token) {
             if (\is_array($token)) {
                 $score += $this->getComplexity($token[0]);
-            } elseif ($token === '?') {
+            } elseif ('?' === $token) {
                 $score += 1;
             }
         }
 
-        return $score === 0 ? 1 : $score;
+        return 0 === $score
+            ? 1
+            : $score;
     }
 
     /**
-     * @param int $code Code of a PHP token.
+     * @param integer $code Code of a PHP token.
      */
     private function getComplexity(int $code): int
     {
         switch ($code) {
-            case T_CLASS:
-            case T_INTERFACE:
-            case T_TRAIT:
-            case 336: // T_ENUM (PHP 8.1)
-            case T_IF:
-            case T_ELSEIF:
-            case T_FOR:
-            case T_FOREACH:
-            case T_WHILE:
-            case T_CASE:
-            case T_CATCH:
-            case T_BOOLEAN_AND:
-            case T_LOGICAL_AND:
-            case T_BOOLEAN_OR:
-            case T_LOGICAL_OR:
+            case \T_CLASS:
+            case \T_INTERFACE:
+            case \T_TRAIT:
+            // T_ENUM (PHP 8.1)
+            case 336:
+            case \T_IF:
+            case \T_ELSEIF:
+            case \T_FOR:
+            case \T_FOREACH:
+            case \T_WHILE:
+            case \T_CASE:
+            case \T_CATCH:
+            case \T_BOOLEAN_AND:
+            case \T_LOGICAL_AND:
+            case \T_BOOLEAN_OR:
+            case \T_LOGICAL_OR:
                 return 1;
-            
             default:
                 return 0;
         }
