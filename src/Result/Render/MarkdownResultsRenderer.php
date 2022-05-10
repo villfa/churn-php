7c1c9bb6671cd7777093833da072bbec4f695afb
Fabien Villepinte <fabien.villepinte@gmail.com>
2022-05-10 09:20:23 +0200

    Add support for markdown

---
+++ src/Result/Render/MarkdownResultsRenderer.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Churn\Result\Render;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @internal
+ */
+final class MarkdownResultsRenderer implements ResultsRendererInterface
+{
+    /**
+     * Renders the results.
+     *
+     * @param OutputInterface $output Output Interface.
+     * @param array<array<float|integer|string>> $results The results.
+     */
+    public function render(OutputInterface $output, array $results): void
+    {
+        $output->write('| File | Times Changed | Complexity | Score |);
+        $output->write('|------|---------------|------------|-------|');
+
+        foreach ($results as $result) {
+            $output->write('| ' . implode(' | ', $result) . ' |');
+        }
+    }
+}
